6f3748329b00edf5a2d1263d9955067969f18da2
Adam McGrade <adam.mcgrade@gmail.com>
2016-12-21 20:13:16 +0800

    WIP - Front Page styles

--- front-page.php
+++ front-page.php
@@ -15,4 +15,23 @@
 //* Force full-width-content layout setting
 add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
 
+//* Remove default content loop
+remove_action( 'genesis_loop', 'genesis_do_loop' );
+
+//* Register Widget Areas
+add_action( 'genesis_before_content', 'green_island_travel_and_tours_homepage_widgets' );
+function green_island_travel_and_tours_homepage_widgets() {
+	// Home Page Hero / Slider
+	genesis_widget_area( 'homepage-hero', array(
+		'before' => '<div class="homepage-hero widget-area">',
+		'after' => '</div>'
+	) );
+
+	// Home Page Newsletter Signup
+	genesis_widget_area( 'homepage-newsletter', array( 
+		'before' => '<div class="horizontal-newsletter-signup homepage-newsletter widget-area">',
+		'after'	 => '</div>'
+	) );
+}
+
 genesis();
--- functions.php
+++ functions.php
@@ -65,6 +65,56 @@ function green_island_travel_and_tours_setup() {
 	unregister_sidebar( 'sidebar-alt' );
 
 
+	//* Register Frontpage Hero Widget Area
+	
+	genesis_register_sidebar( array(
+		'id' => 'homepage-hero',
+		'name' => __( 'Homepage Hero', 'genesis' ),
+		'description' => __( 'Place an image or slider here for display on the home page.', 'green-island-travel-and-tours' )
+	) );
+
+	//* Register Frontpage Newlsetter Widget Area
+	genesis_register_sidebar( array(
+		'id' => 'homepage-newsletter',
+		'name' => __( 'Homepage Newsletter', 'genesis' ),
+		'description' => __( 'Place a newsletter signup form for display on the home page.', 'green-island-travel-and-tours' )
+	) );
+
+
+
+	/**********************************
+	 *
+	 * Replace Header Site Title with Inline Logo
+	 *
+	 * @author AlphaBlossom / Tony Eppright
+	 * @link http://www.alphablossom.com/a-better-wordpress-genesis-responsive-logo-header/
+	 *
+	 * @edited by Sridhar Katakam
+	 * @link https://sridharkatakam.com/use-inline-logo-instead-background-image-genesis/
+	 *
+	************************************/
+	add_filter( 'genesis_seo_title', 'custom_header_inline_logo', 10, 3 );
+	function custom_header_inline_logo( $title, $inside, $wrap ) {
+
+		$logo = '<img src="' . get_stylesheet_directory_uri() . '/images/green_island_travel_and_tours_logo@2x.png" alt="' . esc_attr( get_bloginfo( 'name' ) ) . ' Homepage" width="200" height="100" />';
+
+		$inside = sprintf( '<a href="%s">%s<span class="screen-reader-text">%s</span></a>', trailingslashit( home_url() ), $logo, get_bloginfo( 'name' ) );
+
+		// Determine which wrapping tags to use
+		$wrap = genesis_is_root_page() && 'title' === genesis_get_seo_option( 'home_h1_on' ) ? 'h1' : 'p';
+
+		// A little fallback, in case an SEO plugin is active
+		$wrap = genesis_is_root_page() && ! genesis_get_seo_option( 'home_h1_on' ) ? 'h1' : $wrap;
+
+		// And finally, $wrap in h1 if HTML5 & semantic headings enabled
+		$wrap = genesis_html5() && genesis_get_seo_option( 'semantic_headings' ) ? 'h1' : $wrap;
+
+		return sprintf( '<%1$s %2$s>%3$s</%1$s>', $wrap, genesis_attr( 'site-title' ), $inside );
+
+	}
+
+
+
 	// Enqueue Scripts and Styles
 	add_action( 'wp_enqueue_scripts', 'custom_enqueue_scripts_styles' );
 	function custom_enqueue_scripts_styles() {
